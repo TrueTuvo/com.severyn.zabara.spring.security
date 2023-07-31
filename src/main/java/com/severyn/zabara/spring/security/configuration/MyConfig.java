5fb5d96f1c67cb3277fda5ec413542e3039acaac
Severyn <TrueTuvo@gmail.com>
2023-07-31 16:15:50 +0300

    Implemented basic encryption functionality

--- src/main/java/com/severyn/zabara/spring/security/configuration/MyConfig.java
+++ src/main/java/com/severyn/zabara/spring/security/configuration/MyConfig.java
@@ -1,11 +1,15 @@
 package com.severyn.zabara.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.severyn.zabara.spring.security")
@@ -22,5 +26,18 @@ public class MyConfig {
         return internalResourceViewResolver;
     }
 
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
 
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
 }
--- src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
+++ src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <body>
@@ -7,15 +8,19 @@
 <br>
 <br>
 <br>
-<input type="button" value="Salary"
-    onclick="window.location.href = 'hr_info'">
-Only for HR staff
+<security:authorize access="hasRole('HR')">
+    <input type="button" value="Salary"
+           onclick="window.location.href = 'hr_info'">
+    Only for HR staff
+</security:authorize>
 <br>
 <br>
 <br>
-<input type="button" value="Performance"
-       onclick="window.location.href = 'manager_info'">
-Only for HR managers
+<security:authorize access="hasRole('MANAGER')">
+    <input type="button" value="Performance"
+           onclick="window.location.href = 'manager_info'">
+    Only for HR managers
+</security:authorize>
 
 <br>
 </body>
